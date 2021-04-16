aaccd57cd5e1e46d6feae47d8964f19e50774cbc
Kenneth Mitra <kennethmitra@gmail.com>
2021-04-16 15:23:36 -0500

    Changed logger to a class and added retraining but its still buggy

--- tester.py
+++ tester.py
@@ -5,77 +5,83 @@ import os
 from PIL import Image
 import time
 import argparse
+from pathlib import Path
 import sys
+import glob
 
-# TODO: add argument parser
-parser = argparse.ArgumentParser()
-# TODO: add one argument for selecting PyTorch or TensorFlow option of the code
-parser.add_argument("--fraction", help="[0.05, 0.25, 0.5, 0.75, 0.9]", type=float)
-# TODO: add one argument for selecting VGG or MobileNet-v1 models
-parser.add_argument("--epoch", help="[0, 3, 5]", type=int)
-# TODO: Modify the rest of the code to use those arguments correspondingly
-args = parser.parse_args()
 
-onnx_model_name = f'onnx_models/model_epoch_{args.epoch}_frac_{args.fraction}.onnx'
+if __name__ == '__main__':
 
-# Create Inference session using ONNX runtime
-sess = onnxruntime.InferenceSession(onnx_model_name)
+    LOG_DIR = 'logs'
+    ONNX_MODEL_DIR = "onnx_models"
+    Path(LOG_DIR).mkdir(exist_ok=True, parents=True)
+    assert(Path(ONNX_MODEL_DIR).exists())
 
-# Get the input name for the ONNX model
-input_name = sess.get_inputs()[0].name
-print("Input name  :", input_name)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--fraction", help="[0.05, 0.25, 0.5, 0.75, 0.9]", type=float)
+    # parser.add_argument("--epoch", help="[0, 3, 5]", type=int)
+    # args = parser.parse_args()
 
-# Get the shape of the input
-input_shape = sess.get_inputs()[0].shape
-print("Input shape :", input_shape)
+    for onnx_model_name in glob.glob(f"{ONNX_MODEL_DIR}/*.onnx"):
 
-# Mean and standard deviation used for PyTorch models
-mean = np.array((0.4914, 0.4822, 0.4465))
-std = np.array((0.2023, 0.1994, 0.2010))
+        #onnx_model_name = f'{ONNX_MODEL_DIR}/model_epoch_{args.epoch}_frac_{args.fraction}.onnx'
 
-# Label names for CIFAR10 Dataset
-label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        # Create Inference session using ONNX runtime
+        sess = onnxruntime.InferenceSession(onnx_model_name)
 
-num_correct = 0
-runtime = 0
+        # Get the input name for the ONNX model
+        input_name = sess.get_inputs()[0].name
+        print("Input name  :", input_name)
 
-print(f'-----------RUNNING {onnx_model_name}-----------')
+        # Get the shape of the input
+        input_shape = sess.get_inputs()[0].shape
+        print("Input shape :", input_shape)
 
-# The test_deployment folder contains all 10.000 images from the testing dataset of CIFAR10 in .png format
-for filename in tqdm(os.listdir("test_deployment")):
-    # Take each image, one by one, and make inference
-    with Image.open(os.path.join("test_deployment", filename)).resize((32, 32)) as img:
-        #print("Image shape:", np.float32(img).shape)
+        # Mean and standard deviation used for PyTorch models
+        mean = np.array((0.4914, 0.4822, 0.4465))
+        std = np.array((0.2023, 0.1994, 0.2010))
 
-        # For PyTorch models ONLY: normalize image
-        input_image = (np.float32(img) / 255. - mean) / std
-        # For PyTorch models ONLY: Add the Batch axis in the data Tensor (C, H, W)
-        input_image = np.expand_dims(np.float32(input_image), axis=0)
+        # Label names for CIFAR10 Dataset
+        label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-        # For PyTorch models ONLY: change the order from (B, H, W, C) to (B, C, H, W)
-        input_image = input_image.transpose([0, 3, 1, 2])
+        num_correct = 0
+        runtime = 0
 
-        # Run inference and get the prediction for the input image
-        time_start = time.time()
-        pred_onnx = sess.run(None, {input_name: input_image})[0]
-        runtime += time.time() - time_start
+        print(f'-----------RUNNING {onnx_model_name}-----------')
 
-        # Find the prediction with the highest probability
-        top_prediction = np.argmax(pred_onnx[0])
+        # The test_deployment folder contains all 10.000 images from the testing dataset of CIFAR10 in .png format
+        for filename in tqdm(os.listdir("test_deployment")):
+            # Take each image, one by one, and make inference
+            with Image.open(os.path.join("test_deployment", filename)).resize((32, 32)) as img:
 
-        # Get the label of the predicted class
-        pred_class = label_names[top_prediction]
+                # For PyTorch models ONLY: normalize image
+                input_image = (np.float32(img) / 255. - mean) / std
+                # For PyTorch models ONLY: Add the Batch axis in the data Tensor (C, H, W)
+                input_image = np.expand_dims(np.float32(input_image), axis=0)
 
-        if pred_class in filename:
-            num_correct += 1
+                # For PyTorch models ONLY: change the order from (B, H, W, C) to (B, C, H, W)
+                input_image = input_image.transpose([0, 3, 1, 2])
 
-test_acc = num_correct / len(tqdm(os.listdir("test_deployment")))
-print(f"Test Accuracy: {test_acc}, Runtime: {runtime}")
+                # Run inference and get the prediction for the input image
+                time_start = time.time()
+                pred_onnx = sess.run(None, {input_name: input_image})[0]
+                runtime += time.time() - time_start
 
-log_name = onnx_model_name.replace('.onnx', '')
+                # Find the prediction with the highest probability
+                top_prediction = np.argmax(pred_onnx[0])
 
-with open(f'logs/{log_name}_log.txt', 'a') as f:
-    f.write(f"==========================\n")
-    f.write(f"Model name: {onnx_model_name}\n")
-    f.write(f"Test Accuracy: {test_acc}, Runtime: {runtime}\n")
-    f.write(f"--------------------------\n")
+                # Get the label of the predicted class
+                pred_class = label_names[top_prediction]
+
+                if pred_class in filename:
+                    num_correct += 1
+
+        len_ds = len(tqdm(os.listdir("test_deployment")))
+        test_acc = num_correct / len_ds
+        print(f"Test Accuracy: {test_acc}, Runtime: {runtime}")
+
+        log_name = onnx_model_name.replace('.onnx', '')
+
+        with open(f'{LOG_DIR}/{log_name}_inf_log.csv', 'a') as f:
+            f.write("model_name,test_acc,runtime,avg_latency,num_images\n")
+            f.write(f"{onnx_model_name},{test_acc},{runtime},{runtime/len_ds},{len_ds}\n")
