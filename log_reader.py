341d7bd6b8060d18f4187eb98b4d739c21465f40
Kenneth Mitra <kennethmitra@gmail.com>
2021-04-18 13:45:59 -0500

    Added num_params to table

--- log_reader.py
+++ log_reader.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import glob
 from pathlib import Path
+from onnx_opcounter import calculate_macs, calculate_params
+import onnx
 
 LOG_DIR = 'logs'
 ONNX_MODELS_DIR = 'onnx_models'
 assert Path(LOG_DIR).exists()
 assert Path(ONNX_MODELS_DIR).exists()
 
-table1 = pd.DataFrame(columns=('params', 'mem', 'latency', 'power', 'energy', 'num_images'))
+table1 = pd.DataFrame(columns=('params', 'mem', 'latency', 'power', 'energy', 'num_images', 'num_params'))
 
 for inf_log in glob.glob(f"{LOG_DIR}/*_inf_log.*"):
     # model_name,test_acc,runtime,avg_latency,num_images
     inf_log_df = pd.read_csv(inf_log)
-    new_row = {'params': inf_log_df['model_name'].iloc[0].replace(f'{ONNX_MODELS_DIR}/', '').replace('.onnx', ''), 'mem': None, 'latency': inf_log_df['avg_latency'].iloc[0], 'power': None, 'energy': None, 'num_images': inf_log_df['num_images'].iloc[0]}
+    new_row = {'params': inf_log_df['model_name'].iloc[0].replace(f'{ONNX_MODELS_DIR}/', '').replace('.onnx', ''), 'mem': None, 'latency': inf_log_df['avg_latency'].iloc[0], 'power': None, 'energy': None, 'num_images': inf_log_df['num_images'].iloc[0], 'num_params': None}
     table1 = table1.append(new_row, ignore_index=True)
 
 
@@ -20,6 +22,13 @@ for supl_log in glob.glob(f"{LOG_DIR}/*_supl_log.*"):
     # "run_name time W energy_usage max_mem"
     supl_log_df = pd.read_csv(supl_log, index_col=False)
     table1.loc[table1['params'] == supl_log_df['run_name'].iloc[0], ['mem', 'power', 'energy']] = (supl_log_df['max_mem'].iloc[-1], supl_log_df['W'].max(), supl_log_df['energy_usage'].max()/table1.loc[table1['params'] == supl_log_df['run_name'].iloc[0], 'num_images'])
+
+for model_name in table1['params']:
+    model_path = f'{ONNX_MODELS_DIR}/{model_name}.onnx'
+    print(f"Model: {model_path}")
+    model = onnx.load_model(model_path)
+    num_params = calculate_params(model)
+    table1.loc[table1['params'] == model_name, ['num_params']] = (num_params,)
 
 print(table1)
 table1.to_csv('table1.csv')
