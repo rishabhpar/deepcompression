3642e5d7d51061c82d6598a769ba262deeb7199d
Kenneth Mitra <kennethmitra@gmail.com>
2021-04-17 20:02:27 -0500

    Parsed dem log files

--- log_reader.py
+++ log_reader.py
@@ -3,19 +3,24 @@ import glob
 from pathlib import Path
 
 LOG_DIR = 'logs'
+ONNX_MODELS_DIR = 'onnx_models'
 assert Path(LOG_DIR).exists()
+assert Path(ONNX_MODELS_DIR).exists()
 
 table1 = pd.DataFrame(columns=('params', 'mem', 'latency', 'power', 'energy', 'num_images'))
 
-for inf_log in glob.glob(f"{LOG_DIR}/*_inf_log.csv"):
+for inf_log in glob.glob(f"{LOG_DIR}/*_inf_log.*"):
     # model_name,test_acc,runtime,avg_latency,num_images
     inf_log_df = pd.read_csv(inf_log)
-    new_row = {'params': inf_log_df['model_name'], 'mem': None, 'latency': inf_log_df['avg_latency'], 'power': None, 'energy': None, 'num_images': inf_log_df['num_images']}
+    new_row = {'params': inf_log_df['model_name'].iloc[0].replace(f'{ONNX_MODELS_DIR}/', '').replace('.onnx', ''), 'mem': None, 'latency': inf_log_df['avg_latency'].iloc[0], 'power': None, 'energy': None, 'num_images': inf_log_df['num_images'].iloc[0]}
     table1 = table1.append(new_row, ignore_index=True)
 
 
-for supl_log in glob.glob(f"{LOG_DIR}/*_supl_log.csv"):
+for supl_log in glob.glob(f"{LOG_DIR}/*_supl_log.*"):
     # "run_name time W energy_usage max_mem"
-    supl_log_df = pd.read_csv(supl_log)
-    table1.loc[table1['params'] == supl_log_df['run_name'], ['mem', 'power', 'energy']] = (supl_log_df['max_mem'].iloc[-1], supl_log_df['W'].max(), supl_log_df['energy_usage'].max()/table1.loc[table1['params'] == supl_log_df['run_name'], 'num_images'])
+    supl_log_df = pd.read_csv(supl_log, index_col=False)
+    table1.loc[table1['params'] == supl_log_df['run_name'].iloc[0], ['mem', 'power', 'energy']] = (supl_log_df['max_mem'].iloc[-1], supl_log_df['W'].max(), supl_log_df['energy_usage'].max()/table1.loc[table1['params'] == supl_log_df['run_name'].iloc[0], 'num_images'])
 
+print(table1)
+table1.to_csv('table1.csv')
+table1.to_html('table1.html')
